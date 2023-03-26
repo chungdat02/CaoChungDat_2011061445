aaf45233512339ce2d1b8975bf939345317703fa
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2023-03-26 22:48:17 +0700

    Add files via upload

---
+++ BigSchool/DTOs/AttendanceDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.DTOs
+{
+    public class AttendanceDto 
+    {
+        // GET: AttendanceDto
+        public int CourseId { get; set; }
+    }
+}
